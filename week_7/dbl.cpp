b889e45a3c15cd3f0b0c3653aa226293bd010553
Jan Poonthong <janpoonthong628@gmail.com>
2024-01-03 07:44:33 +0700

    feat: cpp linker

---
+++ week_7/dbl.cpp
@@ -0,0 +1,5 @@
+#include "dbl.h"
+
+int dbl(int n) {
+    return (2 * n);
+}
---
+++ week_7/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+#include "dbl.h"
+
+int main() {
+    cout << dbl(4) << '\n';
+}
